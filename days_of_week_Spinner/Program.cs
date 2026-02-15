1a8211cc2c20498cc21c2da34f2ea081dc8637f4
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-15 00:54:00 -0700

    initial commit

---
+++ days_of_week_Spinner/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+					
+public class Program
+{
+   //write new program logic in the top
+   //at the bottom we are repurposing logic we just made in the previous program
+		
+/* 
+Objective: Combine arrays, Random, and %.
+
+Requirements:
+
+Create a string[] with 7 days: Sun–Sat.
+
+Generate a random number (e.g., 0–99).
+
+Use % 7 to choose the index.
+
+Example Output:
+
+Random number: 15 Day: Monday
+*/
+
+	public static void Main()
+	{
+        Console.WriteLine("Hello world");
+		userInput = Console.ReadLine();
+
+    //int.TryParse is not something you rename every time you use it
+        int.TryParse(userInput, out numberprocess);
+    //you just put a STRING variable in position 1 and OUT it to an int in position 2
+
+        if (numberprocess % 2 == 0) {
+            Console.WriteLine($"{userInput} is Even");
+        }
+        else
+        {
+            Console.WriteLine($"{userInput} is Odd");
+        }
+		
+		
+	}
+}
